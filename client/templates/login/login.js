61caf8d0375a2c6173a7209a0d225f0aa4b45644
Meni Edri <meniedri1@gmail.com>
2016-03-15 02:20:31 +0200

    improvements to register/login forms
    form validations include dealing with error from meteor, different  main
    templates for sign-in uses and guests, temp logo

--- client/templates/login/login.js
+++ client/templates/login/login.js
@@ -1,17 +1,67 @@
+
+Template.login.rendered = function () {
+    this.$("form").on("submit", function (e) { e.preventDefault() });
+}
+
 Template.login.events({
-    'submit form': function(events, template){
+    'submit form': function(event, template){
         event.preventDefault();
+
+        Session.set('formErrors', undefined);
+
         var email = template.find('#email').value;
         var password = template.find('#password').value;
 
         Meteor.loginWithPassword(email, password, function(error){
             if(error){
                 console.log(error.reason);
+                Session.set("formErrors", error.reason);
             } else {
                 FlowRouter.go('/');
             }
         });
-
-
     }
 });
+
+
+Template.login.helpers({
+    formErrors: function() {
+        var formErrors = Session.get("formErrors");
+        return formErrors;
+    }
+});
+
+
+
+Template.login.rendered = function(){
+
+    // reset session var for form errors
+    Session.set('formErrors', undefined);
+
+    $('.loginForm').form({
+        fields: {
+            email: {
+                identifier: 'email',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter your username or email.'
+                    }
+                ]
+            },
+            password: {
+                identifier: 'password',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter your password.'
+                    },
+                    {
+                        type: 'length[6]',
+                        prompt: 'Your password must be at least 6 characters.'
+                    }
+                ]
+            }
+        }
+    });
+};
--- client/templates/register/register.js
+++ client/templates/register/register.js
@@ -17,6 +17,64 @@ Template.register.events({
             }
         });
 
-        FlowRouter.go('/');
     }
 });
+
+Template.register.helpers({
+    formErrors: function() {
+        var formErrors = Session.get("formErrors");
+        return formErrors;
+    }
+});
+
+
+
+Template.register.rendered = function(){
+
+    // reset session var for form errors
+    Session.set('formErrors', undefined);
+
+    $('.loginForm').form({
+        fields: {
+            email: {
+                identifier: 'email',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter your email.'
+                    },{
+                        type: 'email',
+                        prompt: 'Please enter a valid email.'
+                    }
+                ]
+            },
+            username: {
+                identifier: 'username',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter your username.'
+                    },
+                    {
+                        type: 'length[3]',
+                        prompt: 'Your username must be at least 3 characters.'
+                    }
+                ]
+            },
+            password: {
+                identifier: 'password',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter your password.'
+                    },
+                    {
+                        type: 'length[5]',
+                        prompt: 'Your password must be at least 5 characters.'
+                    }
+                ]
+            }
+        }
+    });
+};
+
