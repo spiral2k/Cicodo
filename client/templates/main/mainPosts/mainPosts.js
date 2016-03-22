3abc99c4e308314f9704d6eafda9b673c574c95d
spiral2k <Hhbadmin@gmail.com>
2016-03-22 17:58:38 +0200

    new main posts loader, change profile JS stracture

--- client/templates/main/mainPosts/mainPosts.js
+++ client/templates/main/mainPosts/mainPosts.js
@@ -1,43 +1,42 @@
-Template.mainPosts.onDestroyed(function(){
-     Session.set('mainPostsLoadLimit', 5);
-});
+Template.mainPosts.onCreated(function(){
 
-Template.mainPosts.onRendered(function(){
     Session.set('mainPostsLoadLimit', 5);
 
+    // get user followers array that contain ids;
+    var followArray = Meteor.user().profile.follow;
+
+    if(followArray) {
+        //Subscribe to user followed posts
+        for ( var i = 0; i < followArray.length; i++ ) {
+            // User data
+            this.posts = this.subscribe('usersFollowedByUser', followArray[i]);
+            // Post data
+            this.posts = this.subscribe('postsFollowedByUser', followArray[i]);
+        }
+        //subscribe to Meteor.user() posts.
+        this.posts = this.subscribe('postsFollowedByUser', Meteor.userId());
+
+    }
+
 });
+
 
 Template.mainPosts.events({
     'click #load-more': function() {
         Session.set('mainPostsLoadLimit', Session.get('mainPostsLoadLimit') + 20);
     }
-
 });
 
 Template.mainPosts.helpers({
+    mainPostsReady:function(){
+        return Template.instance().posts.ready();
+    },
     posts: function () {
-        var followArray = Meteor.user().profile.follow;
-
-        if(followArray) {
-            //Subscribe to user followed posts
-            for (var i = 0; i < followArray.length; i++) {
-                // Post data
-                Meteor.subscribe('postsFollowedByUser', followArray[i]);
-
-                // User data
-                Meteor.subscribe('usersFollowedByUser', followArray[i]);
-            }
-        }
-
-        Meteor.subscribe('postsFollowedByUser', Meteor.userId());
-
-        return Posts.find({},{limit: Session.get('mainPostsLoadLimit'), sort:{'createdAt.date': -1}});
-
-
+        var postsList = Posts.find({},{limit: Session.get('mainPostsLoadLimit'), sort:{'createdAt.date': -1}});
+        return postsList;
     },
     username: function(userId) {
         var user = Meteor.users.findOne(userId);
-
         if(user)
             return user.username;
     },
@@ -46,12 +45,14 @@ Template.mainPosts.helpers({
         var user = Meteor.users.findOne(userId);
 
         if(typeof user !== 'undefined')
-            if(typeof  user.profile !== 'undefined' || typeof user.profile.avatar !== 'undefined')
+            if((typeof  user.profile !== 'undefined' || typeof user.profile.avatar !== 'undefined') && user.profile.avatar != "") {
                 return user.profile.avatar;
-        else
-            return "path to default avatar";
+            } else {
+                return "path to default avatar";
+            }
     },
     postsCount: function(){
+        //check if need to show 'Load more posts'.
         if(Posts.find().count() > Session.get('mainPostsLoadLimit')){
             return true
         }else{
@@ -60,4 +61,9 @@ Template.mainPosts.helpers({
 
     }
 
+});
+
+
+Template.mainPosts.onDestroyed(function(){
+    Session.set('mainPostsLoadLimit', 5);
 });
