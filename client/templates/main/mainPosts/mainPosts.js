2814ca3340a9de660c9bb165668cd67ebcece3dd
Meni Edri <meniedri1@gmail.com>
2016-03-24 00:12:37 +0200

    live/regular feed fixs
    now the main posts feed type button is listening to
    user.profile.feedtype

--- client/templates/main/mainPosts/mainPosts.js
+++ client/templates/main/mainPosts/mainPosts.js
@@ -1,31 +1,31 @@
+var user;
+
 Template.mainPosts.onCreated(function(){
 
-
     this.autorun(function(){
+        user = Meteor.user();
 
-        var user = Meteor.user();
-
-        Session.set('feedType', user.profile.feedType);
+        if(user) {
+            Session.set('feedType', user.profile.feedType);
+        }
 
     });
-
-
 
 });
 
 
 Template.mainPosts.onRendered(function(){
+
     $('.ui.dropdown').dropdown({ onChange: function(value, text, $selectedItem) {
-
-
         if(text.indexOf("Live") > -1){
             Session.set('feedType', 'liveFeed');
         }else if(text.indexOf("Regular") > -1){
             Session.set('feedType', 'regularFeed');
         }
-
     }
     });
+    if(user)
+        $('#feedType').dropdown('set selected', user.profile.feedType);
 
 });
 
