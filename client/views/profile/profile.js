0177ac28b4f0c5795ca179618c7209388e78214b
Meni Edri <meniedri1@gmail.com>
2016-05-06 16:47:52 +0300

    profile cover drag fix

--- client/views/profile/profile.js
+++ client/views/profile/profile.js
@@ -1,4 +1,3 @@
-var pos = 0;
 
 Template.profile.onCreated(function() {
 
@@ -6,7 +5,6 @@ Template.profile.onCreated(function() {
     Session.set("coverEdit", false);
     Session.set("CoverImageBase64", false);
     Session.set("profileCurrentPage", "posts");
-
 
     var self = this;
     self.autorun(function() { // Stops all current subscriptions
@@ -17,37 +15,18 @@ Template.profile.onCreated(function() {
         self.subscribe('getUserProfileDataByUsername', username);
         self.subscribe('getUserPostsByUsername', 5);
     });
+
 });
 
 
 Template.profile.onRendered(function(){
 
-
-    Meteor.autorun(function(){
-
-        
-    })
-    Tracker.afterFlush(function () {
-        // Use Tracker.afterFlush to wait for the UI to re-render
-        // then use highlight.js to highlight a code snippet
-        highlightBlock(template.find('.code'));
+    Template.instance().subscribe('getUserPostsByUsername', 5, function(){
+        Tracker.afterFlush(function () {
+                $('#profile-cover-image').backgroundDraggable({bound: false, axis: 'y'});
+        });
     });
 
-    setTimeout(function()
-    {
-    $(".profile-cover-image")
-        .mousedown(function(e){
-            $(this).on("mousemove",function(e){
-                var p1 = { x: e.pageX, y: e.pageY };
-                var p0 = $(this).data("p0") || p1;
-                console.log( p0.y - p1.y);
-                $(this).data("p0", p1);
-            });
-        })
-        .mouseup(function(){
-            $(this).off("mousemove");
-        });
-    }, 0);
 
     $("body").on('change','#CoverUpload' , function(){
 
@@ -61,8 +40,6 @@ Template.profile.onRendered(function(){
                 var srcData = fileLoadedEvent.target.result; // <--- data: base64
 
                 Session.set("CoverImageBase64", srcData);
-
-
                 $("#profile-cover-image").attr("style", "background-image: url(" + srcData + ")");
 
             };
@@ -116,21 +93,7 @@ Template.profile.events({
     'mouseleave .user-edit-avatar':function(){
         $('.edit-avatar-mask').hide();
     },
-    'click #profile-cover-image': function (evt) {
-        // only if in edit mode
 
-        if(Session.get("coverEdit")) {
-
-            if (evt.drag.type === 'dragstart') {
-                console.log('You start dragging! ', evt.drag)
-            } else if (evt.drag.type === 'dragend') {
-                console.log('You stopped dragging!')
-            } else if (evt.drag.type === 'dragging') {
-                pos += evt.drag.dy;
-                $("#profile-cover-image").css("background-position-y", pos + "px");
-            }
-        }
-    },
     'click .profileEdit': function(){
         Session.set("profileEdit", true);
     },
@@ -149,7 +112,11 @@ Template.profile.events({
         Session.set("coverEdit", false);
     },
     'click .finishCoverEdit': function(){
+
         Session.set("coverEdit", false);
+
+        var pos = $("#profile-cover-image").css("background-position-y").match(/\d/g).join("");
+
         if(Session.get("CoverImageBase64")) {
             Meteor.call("updateCoverImage", pos, Session.get("CoverImageBase64"));
         }
@@ -324,3 +291,163 @@ Template.profile.onDestroyed(function(){
     Session.set("CoverImageBase64", false);
     Session.set("profileCurrentPage", null);
 });
+
+
+
+
+
+
+/* DRAG */
+;(function($) {
+    var $window = $(window);
+
+    // Helper function to guarantee a value between low and hi unless bool is false
+    var limit = function(low, hi, value, bool) {
+        if (arguments.length === 3 || bool) {
+            if (value < low) return low;
+            if (value > hi) return hi;
+        }
+        return value;
+    };
+
+    // Adds clientX and clientY properties to the jQuery's event object from touch
+    var modifyEventForTouch = function(e) {
+        e.clientX = e.originalEvent.touches[0].clientX;
+        e.clientY = e.originalEvent.touches[0].clientY;
+    };
+
+    var getBackgroundImageDimensions = function($el) {
+        var bgSrc = ($el.css('background-image').match(/^url\(['"]?(.*?)['"]?\)$/i) || [])[1];
+        if (!bgSrc) return;
+
+        var imageDimensions = { width: 0, height: 0 },
+            image = new Image();
+
+        image.onload = function() {
+            if ($el.css('background-size') == "cover") {
+                var elementWidth = $el.innerWidth(),
+                    elementHeight = $el.innerHeight(),
+                    elementAspectRatio = elementWidth / elementHeight;
+                imageAspectRatio = image.width / image.height,
+                    scale = 1;
+
+                if (imageAspectRatio >= elementAspectRatio) {
+                    scale = elementHeight / image.height;
+                } else {
+                    scale = elementWidth / image.width;
+                }
+
+                imageDimensions.width = image.width * scale;
+                imageDimensions.height = image.height * scale;
+            } else {
+                imageDimensions.width = image.width;
+                imageDimensions.height = image.height;
+            }
+        };
+
+        image.src = bgSrc;
+
+        return imageDimensions;
+    };
+
+    function Plugin(element, options) {
+        this.element = element;
+        this.options = options;
+        this.init();
+    }
+
+    Plugin.prototype.init = function() {
+        var $el = $(this.element),
+            bgSrc = ($el.css('background-image').match(/^url\(['"]?(.*?)['"]?\)$/i) || [])[1],
+            options = this.options;
+
+        if (!bgSrc) return;
+
+        // Get the image's width and height if bound
+        var imageDimensions = { width: 0, height: 0 };
+        if (options.bound) {
+            imageDimensions = getBackgroundImageDimensions($el);
+        }
+
+        $el.on('mousedown.dbg touchstart.dbg', function(e) {
+
+            if (!$(e.target).hasClass('cover-upload-button')) {
+                return;
+            }
+            e.preventDefault();
+
+            if (e.originalEvent.touches) {
+                modifyEventForTouch(e);
+            } else if (e.which !== 1) {
+                return;
+            }
+
+            var x0 = e.clientX,
+                y0 = e.clientY,
+                pos = $el.css('background-position').match(/(-?\d+).*?\s(-?\d+)/) || [],
+                xPos = parseInt(pos[1]) || 0,
+                yPos = parseInt(pos[2]) || 0;
+
+            $window.on('mousemove.dbg touchmove.dbg', function(e) {
+                e.preventDefault();
+
+                if (e.originalEvent.touches) {
+                    modifyEventForTouch(e);
+                }
+
+                var x = e.clientX,
+                    y = e.clientY;
+
+                xPos = options.axis === 'y' ? xPos : limit($el.innerWidth()-imageDimensions.width, 0, xPos+x-x0, options.bound);
+                yPos = options.axis === 'x' ? yPos : limit($el.innerHeight()-imageDimensions.height, 0, yPos+y-y0, options.bound);
+                x0 = x;
+                y0 = y;
+
+
+                    $el.css('background-position', xPos + 'px ' + yPos + 'px');
+
+            });
+
+            $window.on('mouseup.dbg touchend.dbg mouseleave.dbg', function() {
+                if (options.done) {
+                    options.done();
+                }
+
+                $window.off('mousemove.dbg touchmove.dbg');
+                $window.off('mouseup.dbg touchend.dbg mouseleave.dbg');
+            });
+        });
+    };
+
+    Plugin.prototype.disable = function() {
+        var $el = $(this.element);
+        $el.off('mousedown.dbg touchstart.dbg');
+        $window.off('mousemove.dbg touchmove.dbg mouseup.dbg touchend.dbg mouseleave.dbg');
+    }
+
+    $.fn.backgroundDraggable = function(options) {
+        var options = options;
+        var args = Array.prototype.slice.call(arguments, 1);
+
+
+
+            return this.each(function () {
+                var $this = $(this);
+
+                if (typeof options == 'undefined' || typeof options == 'object') {
+                    options = $.extend({}, $.fn.backgroundDraggable.defaults, options);
+                    var plugin = new Plugin(this, options);
+                    $this.data('dbg', plugin);
+                } else if (typeof options == 'string' && $this.data('dbg')) {
+                    var plugin = $this.data('dbg');
+                    Plugin.prototype[options].apply(plugin, args);
+                }
+            });
+
+    };
+
+    $.fn.backgroundDraggable.defaults = {
+        bound: true,
+        axis: undefined
+    };
+}(jQuery));
